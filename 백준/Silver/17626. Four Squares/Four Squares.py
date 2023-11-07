fe7bf8c8c2bf14d0ec3b711d695eb6e11f2d9580
LoLz <45972212+LoLz0309@users.noreply.github.com>
2023-11-07 15:17:15 +0900

    [Silver III] Title: Four Squares, Time: 148 ms, Memory: 110764 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -3,15 +3,19 @@ input = sys.stdin.readline
 
 n = int(input())
 
-rn = [i ** 2 for i in range(225)]
-dp = [0]
+dp = [4] * (n + 1)
+for i in range(int(n ** 0.5) + 1):
+    dp[i ** 2] = 1
+
+if dp[n] == 1:
+    print(1)
+    exit()
 
 for i in range(1, n + 1):
-    dp.append(4)
+    if dp[i] == 1:
+        continue
 
-    j = 1
-    while rn[j] <= i:
-        dp[i] = min(dp[i], dp[i - rn[j]] + 1)
-        j += 1
+    for j in range(1, int(i ** 0.5) + 1):
+        dp[i] = min(dp[i], dp[i - (j ** 2)] + 1)
 
 print(dp[n])
