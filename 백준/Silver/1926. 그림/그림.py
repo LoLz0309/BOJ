b4bd5a075fdbeae4a2e688c785d39e9e6f96f8f8
LoLz <45972212+LoLz0309@users.noreply.github.com>
2023-11-22 21:25:40 +0900

    [Silver I] Title: 그림, Time: 488 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -7,18 +7,17 @@ dxy = ((0, -1), (0, 1), (-1, 0), (1, 0))
 
 n, m = map(int, input().split())
 paper = [list(map(int, input().split())) for _ in range(n)]
-visited = [[0] * m for _ in range(n)]
 
 cnt, large = 0, 0
 
 for r in range(n):
     for c in range(m):
-        if not paper[r][c] or visited[r][c]:
+        if not paper[r][c]:
             continue
 
         size = 0
         queue = deque([(r, c)])
-        visited[r][c] = 1
+        paper[r][c] = 0
         while queue:
             x, y = queue.popleft()
             size += 1
@@ -27,10 +26,10 @@ for r in range(n):
                 nx, ny = x + dx, y + dy
                 if not 0 <= nx < n or not 0 <= ny < m:
                     continue
-                if not paper[nx][ny] or visited[nx][ny]:
+                if not paper[nx][ny]:
                     continue
                 queue.append((nx, ny))
-                visited[nx][ny] = 1
+                paper[nx][ny] = 0
 
         cnt += 1
         large = max(large, size)
